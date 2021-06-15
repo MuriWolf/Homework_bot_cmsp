b1fb8f39705ed51760ab715c4977b222d814c164
MuriWolf <murillop.o06@gmail.com>
2021-06-15 16:46:29 -0300

    Novo códido encima do antigo, agora ele esta menos automatico, mas com menos bugs

--- Automacao.py
+++ Automacao.py
@@ -68,6 +68,7 @@ def entrar_html(codigo, x=0, y=0):
     pyautogui.write(codigo)
     time.sleep(1.5)
     pyautogui.press("f12")
+    time.sleep(1)
     if x == 0 and y == 0:
         pass
     else:
@@ -100,6 +101,103 @@ def selecionar_alternativa(qtde_alternativas, alternativa):
 
         cont = cont + 1
 
+# pyautogui.alert(
+#     "mova o mouse para o canto superior direito se quiser encerrar o programa",
+#     "O programa ira começar!",
+# )
+
+# # abrir o chrome
+# pyautogui.hotkey("win", "s")
+# time.sleep(1)
+# pyautogui.write("chrome")
+# time.sleep(1)
+# enter()
+# time.sleep(4)
+
+# # entrar no csmp
+# pyautogui.write("https://cmspweb.ip.tv/")
+# enter()
+# time.sleep(3)
+
+# # entrar em turmas
+# clicar_em(1085, 127)
+# time.sleep(1)
+
+# # abrir a turma especifica
+# clicar_em(824, 218)
+# time.sleep(3.5)
+
+# # abrir tarefas
+# clicar_em(1098, 134)
+# time.sleep(5)
+
+# while True:
+#     entrar_licao(4)
+
+#     # copiar a perguta
+#     pyautogui.hotkey("ctrl", "a")
+#     copiar()
+#     time.sleep(1)
+
+#     # pesquisar a pergunta
+#     abrir_pagina()
+#     time.sleep(2)
+
+#     colar()
+#     enter()
+#     time.sleep(1.5)
+
+#     # entrar no site
+#     clicar_em(0, 400)
+#     time.sleep(1)
+#     tab()
+#     enter()
+#     time.sleep(1.5)
+
+#     # ir nas respostas do brainly
+#     entrar_html(
+#         r"""//*[@id="question-sg-layout-container"]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div/div/div"""
+#     )
+#     time.sleep(4)
+
+#     # selecionar a alternativa
+#     fechar_pagina()
+#     time.sleep(1.5)
+#     clicar_em(1340, 548)
+#     pyautogui.tripleClick(1356, 686)
+#     pyautogui.tripleClick(1356, 686)
+#     time.sleep(1)
+
+#     entrar_html("question-choice",871, 532)
+
+#     # verificar qual alternativa escolher
+
+#     qtde_alternativas = pyautogui.prompt("Quantas questoes tem?\n(digite de 3 a 5)", "Quatidade de questões")
+
+#     alternativa = pyautogui.prompt(
+#         "Escreva qual é a alternativa...\nSe estiver em dúvida volte ao site da resposta e reveja",
+#         "Confirmação da resposta".casefold(),
+#     )
+
+#     # tratamento de erro
+#     if alternativa not in letras:
+#         sair_confirmar = pyautogui.confirm(
+#             "Aperte em 'ok' para encerrar ou 'cancelar' para continuar o programa",
+#             "Deseja sair?",
+#         )
+
+#         if sair_confirmar == "OK":
+#             pyautogui.alert("Até mais :)", "Programa encerrado")
+#             break
+#         else:
+#             continue
+    
+#     time.sleep(1.5)
+#     selecionar_alternativa(int(qtde_alternativas), str(alternativa))
+#     time.sleep(1)
+#     mandar_licao()
+#     time.sleep(4)
+
 pyautogui.alert(
     "mova o mouse para o canto superior direito se quiser encerrar o programa",
     "O programa ira começar!",
@@ -108,7 +206,7 @@ pyautogui.alert(
 # abrir o chrome
 pyautogui.hotkey("win", "s")
 time.sleep(1)
-pyautogui.write("edge")
+pyautogui.write("chrome")
 time.sleep(1)
 enter()
 time.sleep(4)
@@ -116,8 +214,18 @@ time.sleep(4)
 # entrar no csmp
 pyautogui.write("https://cmspweb.ip.tv/")
 enter()
-time.sleep(3)
+time.sleep(2)
 
+clicar_em(695, 477)
+time.sleep(1)
+clicar_em(747, 378)
+pyautogui.press("3")
+time.sleep(1)
+clicar_em(685, 451)
+clicar_em(662, 493)
+enter() 
+
+time.sleep(3)
 # entrar em turmas
 clicar_em(1085, 127)
 time.sleep(1)
@@ -130,75 +238,44 @@ time.sleep(3.5)
 clicar_em(1098, 134)
 time.sleep(5)
 
-cont = 0
 while True:
-    cont = cont + 1
-    # abrir a lição
-    if cont != 111:
-        entrar_licao(4)
-    # else:
-    #     entrar_licao(3)
+    entrar_licao(4)
 
     # copiar a perguta
-    pyautogui.hotkey("ctrl", "a")
-    copiar()
-    time.sleep(1)
+    pergunta = pyautogui.confirm("copie a questao e aperte em pronto", "continuar")
+    if pergunta == "OK":
+        pyautogui.hotkey("ctrl", "t")
+        time.sleep(1)
+        colar()
+        enter()
+        time.sleep(2)
 
-    # pesquisar a pergunta
-    abrir_pagina()
-    time.sleep(2)
+        # entrar no site
+        clicar_em(0, 400)
+        time.sleep(1)
+        tab()
+        enter()
+        time.sleep(1.5)
 
-    colar()
-    enter()
-    time.sleep(1.5)
-
-    # entrar no site
-    clicar_em(0, 400)
-    time.sleep(1)
-    tab()
-    enter()
-    time.sleep(1.5)
-
-    # ir nas respostas do brainly
-    entrar_html(
-        r"""//*[@id="question-sg-layout-container"]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div/div/div"""
-    )
-    time.sleep(4)
-
-    # selecionar a alternativa
-    fechar_pagina()
-    time.sleep(1.5)
-    clicar_em(1340, 548)
-    pyautogui.tripleClick(1356, 686)
-    pyautogui.tripleClick(1356, 686)
-    time.sleep(1)
-
-    entrar_html("question-choice", 872, 515)
-
-    # verificar qual alternativa escolher
-
-    qtde_alternativas = pyautogui.prompt("Quantas questoes tem?\n(digite de 3 a 5)", "Quatidade de questões")
-
-    alternativa = pyautogui.prompt(
-        "Escreva qual é a alternativa...\nSe estiver em dúvida volte ao site da resposta e reveja",
-        "Confirmação da resposta".casefold(),
-    )
-
-    # tratamento de erro
-    if alternativa not in letras:
-        sair_confirmar = pyautogui.confirm(
-            "Aperte em 'ok' para encerrar ou 'cancelar' para continuar o programa",
-            "Deseja sair?",
+        # ir nas respostas do brainly
+        entrar_html(
+            r"""//*[@id="question-sg-layout-container"]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div/div/div"""
         )
+        time.sleep(5)
+        fechar_pagina()
 
-        if sair_confirmar == "OK":
-            pyautogui.alert("Até mais :)", "Programa encerrado")
+    elif pergunta == "Cancel":
+        saida = pyautogui.confirm("Deseja sair?", "Confirmação de saída")
+        if saida == "OK":
             break
-        else:
-            continue
+        continue
+
+    confirmacao = pyautogui.confirm("Clique em ok quando enviar a resposta", "Proxima questao")
+    if confirmacao == "Cancel":
+        saida = pyautogui.confirm("Deseja sair?", "Confirmação de saída")
+        if saida == "OK":
+            break
+    else:
+        time.sleep(1)
+        continue
     
-    time.sleep(1.5)
-    selecionar_alternativa(int(qtde_alternativas), str(alternativa))
-    time.sleep(1)
-    mandar_licao()
-    time.sleep(4)
